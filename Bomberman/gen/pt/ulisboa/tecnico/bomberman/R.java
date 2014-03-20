284b81675ba44f7e6a34d67b9438ba699a73d897
balidani <balijanosdaniel@gmail.com>
2014-03-20 23:50:12 +0000

    Updated the game layout

--- Bomberman/gen/pt/ulisboa/tecnico/bomberman/R.java
+++ Bomberman/gen/pt/ulisboa/tecnico/bomberman/R.java
@@ -21,34 +21,59 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int btn_bomb=0x7f020000;
+        public static final int btn_down=0x7f020001;
+        public static final int btn_left=0x7f020002;
+        public static final int btn_pause=0x7f020003;
+        public static final int btn_quit=0x7f020004;
+        public static final int btn_right=0x7f020005;
+        public static final int btn_up=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
-        public static final int button1=0x7f080005;
-        public static final int editText1=0x7f080006;
-        public static final int imageView1=0x7f080004;
-        public static final int logoPlaceholder=0x7f080007;
-        public static final int playerCountLabel=0x7f080003;
-        public static final int playerNameLabel=0x7f080000;
-        public static final int playerScoreLabel=0x7f080001;
-        public static final int timeLeftLabel=0x7f080002;
+        public static final int action_settings=0x7f080014;
+        public static final int bombButton=0x7f08000e;
+        public static final int button1=0x7f080011;
+        public static final int dashboard=0x7f080000;
+        public static final int downButton=0x7f08000b;
+        public static final int editText1=0x7f080012;
+        public static final int gameGrid=0x7f080009;
+        public static final int leftButton=0x7f08000c;
+        public static final int logoPlaceholder=0x7f080013;
+        public static final int pauseButton=0x7f08000f;
+        public static final int playerCount=0x7f080008;
+        public static final int playerCountLabel=0x7f080002;
+        public static final int playerName=0x7f080005;
+        public static final int playerNameLabel=0x7f080001;
+        public static final int playerScore=0x7f080006;
+        public static final int playerScoreLabel=0x7f080003;
+        public static final int quitButton=0x7f080010;
+        public static final int rightButton=0x7f08000d;
+        public static final int timeLeft=0x7f080007;
+        public static final int timeLeftLabel=0x7f080004;
+        public static final int upButton=0x7f08000a;
     }
     public static final class layout {
         public static final int activity_game=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int activity_score=0x7f030002;
     }
     public static final class menu {
         public static final int game=0x7f070000;
         public static final int main=0x7f070001;
+        public static final int score=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050005;
         public static final int play_button=0x7f050003;
+        public static final int player_count=0x7f050007;
         public static final int player_name=0x7f050002;
-        public static final int title_activity_game=0x7f050004;
+        public static final int player_name_short=0x7f050004;
+        public static final int player_score=0x7f050005;
+        public static final int time_left=0x7f050006;
+        public static final int title_activity_game=0x7f050008;
+        public static final int title_activity_score=0x7f050009;
     }
     public static final class style {
         /** 
