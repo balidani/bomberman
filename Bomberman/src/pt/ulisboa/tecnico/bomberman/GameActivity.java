955b3f38babda5a2c150dbbc37af0f6dfd206d80
gayana06 <gayanachandrasekara@gmail.com>
2014-04-02 04:48:03 +0100

    Robots move randomly now

--- Bomberman/src/pt/ulisboa/tecnico/bomberman/GameActivity.java
+++ Bomberman/src/pt/ulisboa/tecnico/bomberman/GameActivity.java
@@ -1,8 +1,10 @@
 package pt.ulisboa.tecnico.bomberman;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
 import android.graphics.Point;
 import android.view.Display;
@@ -17,37 +19,53 @@ import android.widget.Toast;
 
 public class GameActivity extends Activity {
 	
-	private final int EMPTY=0;
-	private final int WALL=1;
-	private final int OBSTACLE=2;
-	private final int ROBOT=3;
-	private final int PLAYER=4;
+	public final int EMPTY=0;
+	public final int WALL=1;
+	public final int OBSTACLE=2;
+	public final int ROBOT=3;
+	public final int PLAYER=4;
 	
-	private final String SEP_COLON=":";
-	private final String SEP_PIPE="|";
-	private final String SEP_COMMA=",";
-	private final String SLASH="\\";
+	public final String SEP_COLON=":";
+	public final String SEP_PIPE="|";
+	public final String SEP_COMMA=",";
+	public final String SLASH="\\";
+	
+	public final int UP=0;
+	public final int RIGHT=1;
+	public final int DOWN=2;
+	public final int LEFT=3;
+	
+	public final int RANDOM_MOVEMENT_OPTIONS=4;
+	
 	GridLayout gameGrid;
 	int item;
 	BoardInfo boardInfo;
 	CellInfo[][] cellInfo;
+	ArrayList<Location> wallList;
 	ArrayList<Location> obstacleList;
 	ArrayList<Location> robotList;
 	ArrayList<Location> playersList;
 	Location currentUserLocation;
+	Random rand;
+	Handler handler;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_game);		
+		setContentView(R.layout.activity_game);	
+		rand=new Random();
 		boardInfo=new BoardInfo(1);
 		SetGridProperties();
 		SetBoardDimensions(); 
+		wallList=LoadInitElements(boardInfo.wallLocations);
 		obstacleList=LoadInitElements(boardInfo.obstacleLocations);
 		robotList=LoadInitElements(boardInfo.robotLocations);
 		playersList=LoadInitElements(boardInfo.playerLocatios);
 		currentUserLocation=playersList.get(0);
+		handler=new Handler();
 		DisplayInitialBoard();
+		handler.post(robotUpdate);
+
 	}
 
 	@Override
@@ -75,28 +93,54 @@ public class GameActivity extends Activity {
 	public void MoveDown(View view)
 	{
 		MoveElement(PLAYER,currentUserLocation.X+1,currentUserLocation.Y,currentUserLocation);
-	}
+	}	
 	
-	private synchronized void MoveElement(int elementType,int nextX, int nextY,Location currentLocation)
+	
+	public synchronized void MoveElement(int elementType,int nextX, int nextY,Location currentLocation)
 	{
 		if(elementType==PLAYER)
 		{
-			if(nextX>=0 && nextX<boardInfo.rowcount)
+			if(nextX>(boardInfo.rowcount-1))
+				nextX=0;
+			else if(nextX<0)
+				nextX=(boardInfo.rowcount-1);
+			
+			if(nextY>(boardInfo.columncount-1))
+				nextY=0;
+			else if(nextY<0)
+				nextY=(boardInfo.columncount-1);
+			
+			if(cellInfo[nextX][nextY].elementType==EMPTY)
 			{
-				if(nextY>(boardInfo.rowcount-1))
-					nextY=0;
-				else if(nextY<0)
-					nextY=(boardInfo.rowcount-1);
-				
-				if(cellInfo[nextX][nextY].elementType==EMPTY)
-				{
-					cellInfo[nextX][nextY].elementType=PLAYER;
-					cellInfo[nextX][nextY].cellImg.setImageResource(R.drawable.player_1);
-					cellInfo[currentLocation.X][currentLocation.Y].elementType=EMPTY;
-					cellInfo[currentLocation.X][currentLocation.Y].cellImg.setImageResource(R.drawable.empty);	
-					currentLocation.X=nextX;
-					currentLocation.Y=nextY;
-				}
+				cellInfo[nextX][nextY].elementType=PLAYER;
+				cellInfo[nextX][nextY].cellImg.setImageResource(R.drawable.player_1);
+				cellInfo[currentLocation.X][currentLocation.Y].elementType=EMPTY;
+				cellInfo[currentLocation.X][currentLocation.Y].cellImg.setImageResource(R.drawable.empty);	
+				currentLocation.X=nextX;
+				currentLocation.Y=nextY;
+			}
+			
+		}
+		else if(elementType==ROBOT)
+		{
+			if(nextX>(boardInfo.rowcount-1))
+				nextX=0;
+			else if(nextX<0)
+				nextX=(boardInfo.rowcount-1);
+			
+			if(nextY>(boardInfo.columncount-1))
+				nextY=0;
+			else if(nextY<0)
+				nextY=(boardInfo.columncount-1);
+			
+			if(cellInfo[nextX][nextY].elementType==EMPTY)
+			{
+				cellInfo[nextX][nextY].elementType=ROBOT;
+				cellInfo[nextX][nextY].cellImg.setImageResource(R.drawable.robot);
+				cellInfo[currentLocation.X][currentLocation.Y].elementType=EMPTY;
+				cellInfo[currentLocation.X][currentLocation.Y].cellImg.setImageResource(R.drawable.empty);	
+				currentLocation.X=nextX;
+				currentLocation.Y=nextY;
 			}
 		}
 		//TODO: add movements to other elements
@@ -144,13 +188,12 @@ public class GameActivity extends Activity {
         		
         		tmpCellInfo=new CellInfo();
         		tmpCellInfo.cellImg=cell;        		
-        		if((i%2)==1 && (j%2)==1)
-        		{        			
-        			cell.setImageResource(R.drawable.wall);
+        	    if(CheckElementExist(wallList, i, j))
+        	    {
+        	    	cell.setImageResource(R.drawable.wall);
         			tmpCellInfo.elementType=WALL;
-    	        	gameGrid.addView(cell,boardInfo.boardWidth/(boardInfo.columncount+1),boardInfo.boardHeight/boardInfo.rowcount);
-    	        	
-        		}  
+    	        	gameGrid.addView(cell,boardInfo.boardWidth/(boardInfo.columncount+1),boardInfo.boardHeight/boardInfo.rowcount);  	        	
+        	    }
         		else if(CheckElementExist(obstacleList, i, j))
         		{
          			cell.setImageResource(R.drawable.obstacle);
@@ -203,8 +246,68 @@ public class GameActivity extends Activity {
 		}
 		return isExists;
 	}
-}
+	
+	private Runnable robotUpdate=new Runnable()
+	{
+		
+		public void run()
+		{
+			ActivateRobots();
+		}
+		
+		public void ActivateRobots()
+		{	
+			if(robotList.size()>0)
+			{
+				try
+				{
 
+					int randVal;
+					for(int i=0;i<robotList.size();i++)
+					{
+						randVal=rand.nextInt(RANDOM_MOVEMENT_OPTIONS);
+						if(randVal==UP)
+						{
+							MoveElement(ROBOT, robotList.get(i).X-1, robotList.get(i).Y, robotList.get(i));
+							
+						}
+						else if(randVal==RIGHT)
+						{
+							MoveElement(ROBOT, robotList.get(i).X, robotList.get(i).Y+1, robotList.get(i));
+							
+						}
+						else if(randVal==DOWN)
+						{
+							MoveElement(ROBOT, robotList.get(i).X+1, robotList.get(i).Y, robotList.get(i));
+							
+						}
+						else if(randVal==LEFT)
+						{
+							MoveElement(ROBOT, robotList.get(i).X, robotList.get(i).Y-1, robotList.get(i));
+							
+						}
+					
+					}
+				}
+			
+				catch(Exception ex)
+				{
+					//this will happen when main thread remove a robot. This is expected
+					String msg=ex.getMessage();
+				}
+				finally
+				{
+					handler.postDelayed(this, boardInfo.robotSpeed);
+				}		
+			}
+			else
+			{
+				handler.removeCallbacks(this);
+			}
+        }			 
+	};
+
+}
 
 class Location
 {
@@ -226,6 +329,15 @@ class CellInfo
 
 class BoardInfo
 {
+	String levelName;
+	int gameDuration;
+	int explosionTimeOut;
+	int explosionDuration;
+	int explosionRange;
+	int robotSpeed;
+	int pointsPerRobot;
+	int pointsPerOpponent;
+	
 	int rowcount;
 	int columncount;
 	int boardWidth;
@@ -233,6 +345,7 @@ class BoardInfo
 	String obstacleLocations;
 	String robotLocations;
 	String playerLocatios;
+	String wallLocations;
 		
 	public BoardInfo(int level)
 	{
@@ -241,10 +354,17 @@ class BoardInfo
 	
 	public void LoadDataConfig(int level)
 	{
+		//TODO: these data should be filled from a config file.
+		robotSpeed=1000; //this is in milli seconds
+		
 		rowcount=9;
 		columncount=9;
-		obstacleLocations="0:3,5|1:4,6,8|2:0,1,5,8|3:2,4|4:1,3,8|5:0,2|6:1,2,3|7:6";
+		wallLocations="1:1,3,5|3:1,3,5|5:1,3,4|7:1,3,5,7";
+		obstacleLocations="0:3,5|1:4,6,8|2:0,1,5,8|3:2,4|4:1,8|5:0,2|6:1,2,3|7:6";
 		robotLocations="2:3|3:0|4:2";
 		playerLocatios="0:0";
 	}	
 }
+
+
+
